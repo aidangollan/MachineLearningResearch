013a55597e06c67f5273f6e65e0ea4b672c6183a
Aidan Gollan <aidangollan@icloud.com>
2023-09-08 15:41:23 -0400

    marco set up

---
+++ week1/Marco/main.py
@@ -0,0 +1,14 @@
+from sklearn import svm
+import plotly_express as px
+import pandas as pd
+
+def main():
+
+    x_train = None
+    y_train = None
+    clf = svm.SVC(kernel="linear")
+
+
+
+if __name__ == "__main__":
+    main()
